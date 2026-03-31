6038b4aeee201666c087801dca0e0f313d166559
Dale Emasiri <demasiri@gmail.com>
2026-03-31 22:57:39 +0200

    new item compendium

--- scripts/xf2e-ammo.mjs
+++ scripts/xf2e-ammo.mjs
@@ -1,30 +1,27 @@
 const MODULE = "xf2e-ammo"
-const MODULE_WEAPON_RANGES = [5, 10, 20, 30, 40, 50, 60, 80, 100, 120, 150, 200, 300]
-const MODULE_AMMO_TYPES = {
+const WEAPON_RANGES = [ 5, 10, 20, 30, 40, 50, 60, 80, 100, 120, 150, 200, 300 ]
+const AMMO_TYPES = {
 	"shell": {
 		label: `xf2e-ammo.AmmoType.shell`,
-		stackGroup: "shell"
+		stackGround: "shell"
 	},
 	"shell-gunblade": {
 		label: `xf2e-ammo.AmmoType.shell-gunblade`,
 		parent: "shell",
-		stackGroup: "shell-gunblade",
 		magazine: false,
 		weapon: "gunblade"
 	}
 }
-const MELEE_AMMO_TRAIT = "melee-ammo"
 
-function hasMeleeAmmoTrait(source) {
-	const traits = source?.system?.traits?.value
-	return Array.isArray(traits) && traits.includes(MELEE_AMMO_TRAIT)
+function hasMeleeTrait(source) {
+	return Array.isArray(traits) && source.system.traits?.value.includes("melee-ammo")
 }
 
 Hooks.on("preCreateItem", (item, data) => {
 	if (item.type !== "weapon")
 		return
 
-	if (hasMeleeAmmoTrait(data))
+	if (hasMeleeTrait(data))
 		foundry.utils.setProperty(data, "system.attribute", "str")
 })
 
@@ -32,20 +29,20 @@ Hooks.on("preUpdateItem", (item, changed) => {
 	if (item.type !== "weapon")
 		return
 
-	const nextTraits = foundry.utils.getProperty(changed, "system.traits.value") ?? item.system.traints.value
+	const nxTrait = foundry.utils.getProperty(changed, "system.traits.value") ?? item.system.traits.value
 
-	if (!Array.isArray(nextTraits))
+	if (!Array.isArray(nxTrait))
 		return
 
-	if (nextTraits.includes(MELEE_AMMO_TRAIT))
+	if (nxTrait.includes("melee-ammo"))
 		foundry.utils.setProperty(changed, "system.attribute", "str")
 	else if (item.system.attribute == "str")
-		foundry.utils.setPorperty(changed, "system.attribute", null)
+			foundry.utils.setProperty(changed, "system.attribute", null)
 })
 
 function buildWeaponRanges() {
 	return Object.fromEntries(
-		MODULE_WEAPON_RANGES.map((range) => [
+		WEAPON_RANGES.map((range) => [
 			range,
 			game.i18n.format("PF2E.WeaponRangeN", {range})
 		])
@@ -53,10 +50,10 @@ function buildWeaponRanges() {
 }
 
 Hooks.once("setup", () => {
-	if (!CONFIG.PF2E?.ammoTypes)
+	if (!CONFIG.PF2E.ammoTypes)
 		return
 
-	foundry.utils.mergeObject(CONFIG.PF2E.ammoTypes, MODULE_AMMO_TYPES, {
+	foundry.utils.mergeObject(CONFIG.PF2E.ammoTypes, AMMO_TYPES, {
 		inplace: true,
 		insertKeys: true,
 		insertValues: true,
@@ -66,15 +63,17 @@ Hooks.once("setup", () => {
 
 Hooks.once("ready", () => {
 	const cls = CONFIG.Item.sheetClasses.weapon["pf2e.WeaponSheetPF2e"]?.cls
+
 	if (!cls || cls.prototype.__xf2eAmmoPatched)
 		return
 
-	const origGetData = cls.prototype.getData
+	const orig = cls.prototype.getData
 
-	cls.prototype.getData = async function (...args) {
-		const data = await origGetData.apply(this, args)
+	cls.prototpye.getData = async function (...args) {
+		const data = await orig.apply(this, args)
 		data.weaponRanges = buildWeaponRanges()
 		return data
 	}
+
 	cls.prototype.__xf2eAmmoPatched = true
 })
